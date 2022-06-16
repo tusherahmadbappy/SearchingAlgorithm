f316478fbff388ff2e2332d0730824e7b0791f67
Md. Tusher Ahmad Bappy <106505665+tusherahmadbappy@users.noreply.github.com>
2022-06-16 21:13:34 +0600

    Binary search algorithm

---
+++ binarysearch.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+int binary_search(int arr[],int l,int num,int value)
+{
+    int low=l,high=num,item=value,mid;
+
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(arr[mid]==item)
+        {
+           return mid;
+        }
+        else if(item>arr[mid])
+        {
+            low=mid+1;
+        }
+        else
+        {
+             high=mid-1;
+        }
+    }
+    return -1;
+
+}
+
+
+int main()
+{
+    int arr[50],num,i,size,low,high,item,temp,j;
+    cout<<"Enter the total number of array: ";
+    cin>>num;
+
+    for(i=1;i<=num;i++)
+    {
+        cout<<"Number of index[ "<<i<<" ] is: ";
+        cin>>arr[i];
+    }
+    for(int i=1;i<=num;i++)
+    {
+        for(j=i+1;j<=num;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+
+            }
+        }
+    }
+
+
+    cout<<"how value search you want: ";
+    cin>>item;
+
+   int result= binary_search(arr,1,num,item);
+
+   if(result==-1)
+   {
+    cout<<"Item is not found: "<<endl;
+   }
+   else
+   {
+     cout<<"Item is found: "<<result<<endl;
+   }
+
+   return 0;
+
+}
